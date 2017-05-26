1ed7d7e5f6a2eee5803f7dd18f45c40077fb30cc
lvsheng <lvsheng.ls@alibaba-inc.com>
2017-05-26 10:14:24 +0800

    fel,kotlin

---
+++ src/fel/Foo.java
@@ -0,0 +1,28 @@
+package fel;
+
+/**
+ * @author 儒尊
+ * @date 2017/05/18
+ */
+public class Foo {
+	private int size = 10;
+	private String name = "lvsheng";
+
+	public int getSize() {
+		return size;
+	}
+
+	public Foo setSize(int size) {
+		this.size = size;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Foo setName(String name) {
+		this.name = name;
+		return this;
+	}
+}
